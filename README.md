4fa2215ee1eac0f10ae8691f75a1afceee507dea
Ellenszenves <fallouterdo@gmail.com>
2022-07-14 21:11:27 +0200

    crontab script

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # docker-miniapp
 Miniapp with docker-compose
+Cronjob: 0 20 * * * ~/docker-miniapp/reposync.sh >/dev/null 2>&1
---
+++ reposync.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [[ -d "~/docker-miniapp" ]]
+then
+pull=$(git pull)
+	if [[ $pull == "Already up to date." ]]
+	then
+	echo "Minden naprakész!"
+	else
+	cd ~/docker-miniapp
+	docker compose down
+	docker compose up
+	fi
+else
+cd ~
+git clone https://github.com/Ellenszenves/docker-miniapp.git
+cd ~/docker-miniapp
+docker compose up
+fi
